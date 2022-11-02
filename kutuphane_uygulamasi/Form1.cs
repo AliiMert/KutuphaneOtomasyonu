087ffc67a7a0b7dfa9b2d6890926a68b4308da2c
Ali Mert UYUMAZ <72565803+AliiMert@users.noreply.github.com>
2022-11-02 22:14:02 +0300

    Add files via upload

---
+++ kutuphane_uygulamasi/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kutuphane_uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        //SQL kullanıcıların olduğu veritabanı bağlantısı
+        SqlConnection kullanicilarbaglanti = new SqlConnection("Data Source=DESKTOP-TSDL2U5\\MSSQLSERVER3;Initial Catalog=Kutuphane_Kullanicilar;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+            Nick_Hatirla();
+        }
+
+        private void Txt_Box_Parola_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           Txt_Box_Parola.UseSystemPasswordChar = true; //Yazılan şifreyi gizliyorum.
+        }
+
+        private void Chc_Box_Parola_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chc_Box_Parola.CheckState==CheckState.Unchecked) //if sorgusu yapıp check boxın durumunu kontrol ediyorum.if koşulu sağlıyorsa Parolamı göster yazıyor.
+            {
+                Txt_Box_Parola.UseSystemPasswordChar = true;
+                Chc_Box_Parola.Text = "Parolamı Göster";
+            }
+            else if (Chc_Box_Parola.CheckState==CheckState.Checked)
+            {
+                Txt_Box_Parola.UseSystemPasswordChar = false;
+                
+            }
+        }
+
+        private void BTN_Parola_Click(object sender, EventArgs e)
+        {
+            //parola unuttum butonuna tıklandığında parola değiştirme sayfasına geçiyor
+            Form2 form2 = new Form2();
+            form2.Show();
+            
+
+        }
+
+        private void BTN_Yeni_uye_Click(object sender, EventArgs e)
+        {
+            //Yeni üyelik butonuna basıldığında üyelik sayfasına geçiyor
+            YeniUyelik yeniUyelik = new YeniUyelik();
+            yeniUyelik.Show();
+        }
+
+        private void Btn_Giris_Yap_Click(object sender, EventArgs e)
+        {
+            string kullanicinick = Txt_Box_Kullanici_ismi.Text;
+            string kullanicisifre = Txt_Box_Parola.Text;
+            kullanicilarbaglanti.Open();
+            //girilen kullanıcı adındaki şifreyi alır
+            SqlCommand girisyapkomut = new SqlCommand("Select kullanici_sifre From Kutuphane_Kullanicilar Where kullanici_nick = '" + kullanicinick + "'"
+                    , kullanicilarbaglanti);
+            girisyapkomut.ExecuteNonQuery();
+
+           SqlDataReader sifreal = girisyapkomut.ExecuteReader();
+            sifreal.Read();
+            string alinansifre = sifreal["kullanici_sifre"].ToString();
+
+             if(kullanicisifre != alinansifre)
+            {
+                string message = "Lütfen kullanıcı adı veya şifrenizi kontrol edin!";
+                string title = "UYARI";
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);  // yanlış şifre girilirse
+            }
+            else
+            {
+                string message = "Giriş Başarılı!";
+                string title = "BAŞARILI";
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);  // boş yer varsa uyarı verir
+
+                FormKitapKiralama formac = new FormKitapKiralama();
+                formac.Show();
+                Son_Nick_Kaydet();
+            }
+            kullanicilarbaglanti.Close();
+        }
+
+        //Projenin ayarlar kısmına son girlen nick ve beni hatırla tuşunun verileri kaydediliyor
+       private void Nick_Hatirla()
+        {
+            if (Properties.Settings.Default.KullaniciNick != String.Empty)
+            {
+                if (Properties.Settings.Default.NickHatirla == true)
+                {
+                    Txt_Box_Kullanici_ismi.Text = Properties.Settings.Default.KullaniciNick;
+                    Chc_Box_Ad_Hatirla.Checked = true;
+                }
+                else
+                {
+                    Txt_Box_Kullanici_ismi.Text = Properties.Settings.Default.KullaniciNick;
+                }
+            }
+        }
+
+        //Projenin ayarlar kısmından girilen nick ve beni hatırla kayıtlı ise veriler çekilip ekrana yazdırılıyor
+        private void Son_Nick_Kaydet()
+        {
+            if (Chc_Box_Ad_Hatirla.Checked)
+            {
+                Properties.Settings.Default.KullaniciNick = Txt_Box_Kullanici_ismi.Text;
+                Properties.Settings.Default.NickHatirla = true;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Properties.Settings.Default.KullaniciNick = "";
+                Properties.Settings.Default.NickHatirla = false;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
---
+++ kutuphane_uygulamasi/FormKitapKiralama.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kutuphane_uygulamasi
+{
+    public partial class FormKitapKiralama : Form
+    {
+        public FormKitapKiralama()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Kitap_Listele_Click_1(object sender, EventArgs e)
+        {
+            //SQL bağlantısı
+            SqlConnection baglantiKitap = new SqlConnection("Data Source=DESKTOP-TSDL2U5\\MSSQLSERVER3;Initial Catalog=Kitaplar;Integrated Security=True");
+            //SQL sorgusunun metni oluşturuldu
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = "SELECT * FROM kitap_bilgi order by Kitap_Adi ASC";
+            //komutun hangi veritabanında çalışacağı belirtildi
+            komut.Connection = baglantiKitap;
+            //sorgu metni veritabanında çalıştırıldı
+            SqlDataAdapter adap = new SqlDataAdapter(komut);
+            //değerler bir tabloya aktarıldı
+            DataTable tablo = new DataTable();
+            //tablodaki veriler datagridview içine yazıldı
+            adap.Fill(tablo);
+            kitaplar_listesi.DataSource = tablo;
+        }
+
+        private void btn_Kitap_Kirala_Click_1(object sender, EventArgs e)
+        {
+            //alış tarihi değişkenine anlık olarak gözüken tarih atandı
+            string atarih = kiralama_tarihi.Text;
+            string secilensatir = kitaplar_listesi.CurrentRow.Cells[1].Value.ToString();
+            txt_secilen_kitaplar.Text = atarih + " tarihinde " + secilensatir + " kitabını kiraladınız. 15 gün içinde geri teslim etmeniz önemle rica edilir.";
+
+        }
+
+        private void FormKitapKiralama_Load(object sender, EventArgs e)
+        {
+            //ekranda zamanı göstermek için kullanıldı
+            timer1.Start();
+
+            string giren_kullanici_adi = "Ali Mert";
+            txt_giris_metni.Text = "Hoş geldin " + giren_kullanici_adi;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //günün tarihini alıp label içine yazıyor
+            kiralama_tarihi.Text = DateTime.Now.ToString();
+        }
+    }
+}
